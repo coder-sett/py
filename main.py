5712da9d270b7f91eb603fc03f0a8c564428d416
laixingyou <laixy7@gmail.com>
2023-01-18 15:14:13 +0800

    update crawler

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ def format_time(time1):
 def get_results(repo_name):
     url = "https://api.github.com/repos/" + repo_name
     repo_response = requests.get(
-        url, headers={'Accept': 'application/vnd.github.v3+json', 'Authorization': 'token ***'})
+        url, headers={'Connection': 'close', 'Accept': 'application/vnd.github.v3+json', 'Authorization': 'token ***'})
     status_code = repo_response.status_code
-    print("Status code:", status_code)
+    print("Status code:", status_code, url)
     if status_code == 200:
         repo_dict = repo_response.json()
         star = repo_dict['stargazers_count']
@@ -27,7 +27,8 @@ def get_results(repo_name):
         created_at = repo_dict['created_at']
         pushed_at = repo_dict['pushed_at']
         topics = repo_dict['topics']
-        return {'仓库url': 'https://github.com/' + repo_name, 'star数': star, 'fork数': forks, '第一次提交': format_time(created_at), '最近一次提交': format_time(pushed_at),  "topics": topics}
+        description = repo_dict['description']
+        return {'仓库url': 'https://github.com/' + repo_name, 'star数': star, 'fork数': forks, '第一次提交': format_time(created_at), '最近一次提交': format_time(pushed_at),  "topics": topics, "description": description}
     else:
         return {'仓库url': 'https://github.com/' + repo_name}
 
@@ -35,17 +36,16 @@ def get_results(repo_name):
 if __name__ == '__main__':
 
     # 创建或打开文件
-    csvfile = open('仓库信息.csv', mode='w', newline='')
+    csvfile = open('仓库信息2.csv', mode='w', newline='', encoding='utf-8')
     # 标题列表
-    fieldnames = ['star数', 'fork数', '第一次提交', '最近一次提交', '仓库url', "topics"]
+    fieldnames = ['仓库url', 'star数', 'fork数',
+                  '第一次提交', '最近一次提交', "topics", "description"]
     # 创建 DictWriter 对象
     write = csv.DictWriter(csvfile, fieldnames=fieldnames)
     # 写入表头
     write.writeheader()
     data = read_tex("selectedActiveList.txt")
     # data = ['k9mail/k-9']
-    print(data)
-    # list = []
     for i in data:
-        # list.append(get_results(i))
-        write.writerow(get_results(i))
+        write.writerow(get_results(i))   # 写入一行数据
+        time.sleep(0.2)
